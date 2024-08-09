27652caf30ea270897178c85da7db206b02c87f2
zzingo5 <zzingo5@gmail.com>
2024-08-09 22:12:54 +0900

    chater 04

--- tutor-nodb/src/state.rs
+++ tutor-nodb/src/state.rs
@@ -1,6 +1,9 @@
 use std::sync::Mutex;
 
+use crate::models::Course;
+
 pub struct AppState {
     pub health_check_response: String,
     pub visit_count: Mutex<u32>,
+    pub courses: Mutex<Vec<Course>>,
 }
