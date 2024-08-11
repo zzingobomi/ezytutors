bb191af2f05e1a0d8b347540f68ac28b65b2cc61
zzingobomi <zzingo5@naver.com>
2024-08-12 00:39:52 +0900

    iter4

--- tutor-db/src/database.sql
+++ tutor-db/src/database.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS ezy_course_c4;
-CREATE TABLE ezy_course_c4
+DROP TABLE IF EXISTS ezy_course_c5;
+CREATE TABLE ezy_course_c5
 (
     course_id serial PRIMARY KEY,
     tutor_id INT NOT NULL,
@@ -7,5 +7,5 @@ CREATE TABLE ezy_course_c4
     posted_time TIMESTAMP DEFAULT now()
 );
 
-INSERT INTO ezy_course_c4 (course_id, tutor_id, course_name, posted_time) VALUES(1, 1, 'First course', '2020-12-17 05:40:00');
-INSERT INTO ezy_course_c4 (course_id, tutor_id, course_name, posted_time) VALUES(2, 1, 'Second course', '2020-12-18 05:45:00');
+INSERT INTO ezy_course_c5 (course_id, tutor_id, course_name, posted_time) VALUES(1, 1, 'First course', '2021-03-17 05:40:00');
+INSERT INTO ezy_course_c5 (course_id, tutor_id, course_name, posted_time) VALUES(2, 1, 'Second course', '2021-03-18 05:45:00');
